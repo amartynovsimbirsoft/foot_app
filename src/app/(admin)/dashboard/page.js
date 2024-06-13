06c633cefbeb7120be641a8b89abd25f7315b84d
Anton Martynov <anton.martynov@simbirsoft.com>
2024-06-13 19:02:08 +0400

    add new page

---
+++ src/app/(admin)/dashboard/page.js
@@ -0,0 +1,9 @@
+import styles from "../page.module.css";
+
+export default function Matches() {
+  return (
+    <main className={styles.main}>
+        <h1>Страница дашборда</h1>
+    </main>
+  );
+}
